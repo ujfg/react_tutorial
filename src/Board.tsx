81ce701be50c4714d6ef725988d05267b641f2a9
ujfg <ujeong.fg@gmail.com>
2023-02-04 19:16:40 +0900

    StateをGameにリフトアップ

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,56 +1,39 @@
-import { FC, useState } from "react";
-import { calculateWinner, SquareSign } from "./Game";
+import { FC } from "react";
+import { BoardStatus } from "./Game";
 import { Square } from "./Square";
 
-export const Board: FC = () => {
-  const [squares, setSquares] = useState<SquareSign[]>(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState<boolean>(true)
+type BoardProps = {
+  squares: BoardStatus
+  onClick(i: number): void
+}
 
-  const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-    }
-
+export const Board: FC<BoardProps> = (props) => {
   const renderSquare = (i: number) => {
     return (
       <Square 
-        value={squares[i]}
-        onClick={() => handleClick(i)} 
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)} 
       />
     )
   }
 
-  const handleClick = (i: number) => {
-    const copied_squares = squares.slice() // コピー
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    copied_squares[i] = xIsNext ? 'X' : 'O'
-    setSquares(copied_squares)
-    setXIsNext(!xIsNext)
-  }
-
   return (
     <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
   )
 }
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { Board } from "./Board";
 
 export type SquareSign = 'X' | 'O' | null
 export type BoardStatus = SquareSign[]
-export type History = [
-  {boardStatus: BoardStatus}
-]
+export type History = {boardStatus: BoardStatus}[]
 
-export class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
+export const Game: FC = () => {
+  const [history, setHistory] = useState<History>([{boardStatus: Array(9).fill(null)}])
+  const [xIsNext, setXIsNext] = useState<boolean>(true)
+  const currentBoardStatus = history[history.length - 1].boardStatus
+  const winner = calculateWinner(currentBoardStatus);
+  let status: string
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
   }
+
+  const handleClick = (i: number) => {
+    const currentBoardStatus = history[history.length - 1].boardStatus.slice()
+    if (calculateWinner(currentBoardStatus) || currentBoardStatus[i]) {
+      return;
+    }
+    currentBoardStatus[i] = xIsNext ? 'X' : 'O'
+    setHistory(history.concat([{boardStatus: currentBoardStatus}]))
+    setXIsNext(!xIsNext)
+  }
+  
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentBoardStatus} onClick={(i: number) => {handleClick(i)}}/>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
 }
 
 export const calculateWinner = (squares: SquareSign[]) => {
